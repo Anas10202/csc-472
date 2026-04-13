9245907290cb556923fb7733d55a6dbb0d8cdf3c
Anas Ahmed <77549785+Anas10202@users.noreply.github.com>
2026-04-13 15:02:28 -0400

    hw 3b

---
+++ vshader3b1.glsl
@@ -0,0 +1,15 @@
+#version 330
+
+in vec3 a_Position;
+in vec2 a_TexCoord;
+
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+
+out vec2 v_TexCoord;
+
+void main()
+{
+	gl_Position = u_Projection * u_View * vec4(a_Position, 1.0);
+	v_TexCoord = a_TexCoord;
+}
---
+++ vshader3b2.glsl
@@ -0,0 +1,11 @@
+#version 330
+
+in vec3 a_Position;
+
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+
+void main()
+{
+	gl_Position = u_Projection * u_View * vec4(a_Position, 1.0);
+}
