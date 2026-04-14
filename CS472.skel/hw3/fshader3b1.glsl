1d3c37b7a783538fa6ab2afaccdb206e74999c66
Nirath Hussan <silver.burner2@gmail.com>
2026-04-14 16:52:09 -0400

    fix: retrieved anas code files

--- CS472.skel/hw3/fshader3b1.glsl
+++ CS472.skel/hw3/fshader3b1.glsl
@@ -1,8 +1,10 @@
 #version 330
 
-// PUT YOUR CODE HERE
+in vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+out vec4 fragColor;
 
 void main() 
 {
-	// PUT YOUR CODE HERE
+	fragColor = texture(u_Sampler, v_TexCoord);
 }
--- CS472.skel/hw3/vshader3b2.glsl
+++ CS472.skel/hw3/vshader3b2.glsl
@@ -1,8 +1,11 @@
 #version 330
 
-// PUT YOUR CODE HERE
+in vec3 a_Position;
 
-void main() 
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+
+void main()
 {
-	// PUT YOUR CODE HERE
+	gl_Position = u_Projection * u_View * vec4(a_Position, 1.0);
 }
